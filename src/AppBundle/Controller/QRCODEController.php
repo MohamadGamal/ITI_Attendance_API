eb98b6acf137b47163351d4ad298f2f07311055f
Mohamad Gamal <Mohamad.Gamal.abdelhay@gmail.com>
2017-05-20 06:32:02 -0400

    pre-final commit

--- src/AppBundle/Controller/QRCODEController.php
+++ src/AppBundle/Controller/QRCODEController.php
@@ -97,7 +97,7 @@ class QRCODEController extends Controller
 
         $cache = new FilesystemAdapter();
         $refreshinterval=30;
-        $mins=100;
+        $mins=700;
         $now=new \Datetime();
         $start=new \Datetime('7:00:00');
         $end=clone $start;
